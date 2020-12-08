9d923e916a30bcf84f3c93bc7a4b93835ace4f77
Sarthak shukla <sarthak1shukla@gmail.com>
2020-12-08 12:39:58 +0530

    add chnages

--- main.js
+++ main.js
@@ -1,10 +1,20 @@
+function len(){
+    var len1= $('#ul1 > li').length;
+    var len2= $('#ul2 > li').length;
+    $("#todo").text("To-Do Tasks : "+len1);
+    $("#comp").text("Compleated Task : "+len2);
+    $("#pend").text(" Pending Tasks : "+len1);
+    $("#comp1").text("Compleated :"+ len2 );
+}
 $("#btn1").click(function () {
     var inputval = $("#inp").val();
-    $("#ul1").append("<li><input type='checkbox' class='check'> <span class='input1'>"
+    $("#ul1").append("<li class='li11'><input type='checkbox' class='check'> <span class='input1'>"
         + inputval + "</span>  <input type='text' class='inpedit'> <button class='edit'> EDIT </button> <button class='save' style='display:none;'> EDIT </button> <button class='del'> DELETE </button> </li>");
     $("#inp").val("");
+    len();
     $(".del").click(function () {
         $(this).parent('li').remove();
+        len();
     });
     $(".edit").click(function () {
         var y = $(this).siblings('.input1').text();
@@ -14,7 +24,7 @@ $("#btn1").click(function () {
     $(".save").click(function () {
         var z = $(this).siblings('.inpedit').val();
         $(this).parent('li').find(".input1").text(z);
-        $(".save").hide() && $(".edit").show();        
+        $(".save").hide() && $(".edit").show();       
     });
     $(".check").click(function () {
         var x = $(this).parent('li');
@@ -24,5 +34,6 @@ $("#btn1").click(function () {
         if ($(this).prop("checked") == false) {
             $("#ul1").append(x);
         }
+        len();
     });
 });
